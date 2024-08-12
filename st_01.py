c3d5f381035d8455096ad04986f8d430c9fa8917
park <op1144@naver.com>
2024-08-12 13:43:13 +0900

    first01

---
+++ st_01.py
@@ -0,0 +1,27 @@
+#데이타타입[문자열]
+
+v_str01 = "niceman"
+v_bool = True
+v_str02 = "good boy"
+v_folat = 10.3
+v_int = 7
+v_dict ={
+    "name" : "kim", 
+    "age" : 25
+}
+v_list = [ 3,5,7]
+v_tuple = 3, 5, 7
+v_set = {7,8,9}
+
+
+
+print(type(v_str01))
+print(type(v_bool))
+print(type(v_str02))
+print(type(v_folat))
+print(type(v_int))
+print(type(v_dict))
+print(type(v_list))
+print(type(v_tuple))
+print(type(v_set))
+
