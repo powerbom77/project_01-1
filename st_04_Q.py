265d1c90fe86d81107cb6046a831dcadf3d76d59
park <op1144@naver.com>
2024-08-16 14:07:27 +0900

    영상과틀리나와서 맨붕

--- st_04_Q.py
+++ st_04_Q.py
@@ -44,29 +44,50 @@ print(re.sub('[^0-9]','',a7))
 
 # 8. 다음 문자열(URL)에서 "http://" 부분을 제거 후 출력하세요. : "http://daum.net"
 
+wd01 = "http://daum.net"
+
+print('정답은: ',wd01[7:])
 
 # 9. 다음 문자열을 모두 대문자, 소문자로 각각 출력해보세요. : "NiceMan"
-
+str = "NiceMan"
+print(str.upper())
 
 # 10. 다음 문자열을 슬라이싱을 이용해서 "cde"만 출력하세요. : "abcdefghijklmn"
-
+str = "abcdefghijklmn"
+print(str[2:5])
 
 # 11. 다음 리스트에서 "Apple" 항목만 삭제하세요. : ["Banana", "Apple", "Orange"]
+list = ["Banana", "Apple", "Orange"]
+list.remove("Apple")
+print(list)
+
 
 
 # 12. 다음 튜플을 리스트로 변환하세요. : (1,2,3,4)
 
+tup = (1, 2, 3, 4)
+print([s for s in tup])
+
 
 # 13. 다음 항목을 딕셔너리(dict)으로 선언해보세요. : <성인 - 100000 , 청소년 - 70000 , 아동 - 30000>
 
-
+qq01_dict = {}
+qq01_dict['성인'] = 100000
+qq01_dict['청소년'] = 70000
+qq01_dict['아동'] = 30000
+print(qq01_dict)
 # 14. 13번 에서 선언한 dict 항목에 <소아 - 0> 항목을 추가해보세요.
 
 
+qq01_dict['소아']=0
+print(qq01_dict)
 # 15. 13번에서 선언한 딕셔너리(dict)에서 Key 항목만 출력해보세요.
+#print(dict.keys())
+
+print(qq01_dict.keys())
 
 
 # 16. 13번에서 선언한 딕셔너리(dict)에서 value 항목만 출력해보세요.
-
+print(qq01_dict.values())
 
 # *** 결과 값만 정확하게 출력되면 됩니다. ^^* 고생하셨습니다. ***
